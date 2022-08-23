8d57f00a43e00b51cdb43f87530bbe33fac0cd34
tato <tato.ocampo@live.com.ar>
2022-08-23 13:55:13 -0300

    Landing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import Navbar from "./components/Navbar/Navbar"
 import {ItemListContainer} from './components/Main/ItemListContainer'
 
-function App() {
+function App(props) {
   return (
     <>
       <Navbar></Navbar>
-      <ItemListContainer></ItemListContainer>
+      <ItemListContainer mensaje="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+      anim id est laborum">
+      </ItemListContainer>
     </>
   );
 }
