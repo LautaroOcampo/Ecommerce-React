946962a7856dda8bb19cdccca9a1c0bb0bc0d0cb
tato <tato.ocampo@live.com.ar>
2022-09-04 20:05:22 -0300

    Primera entrega del proyecto final

--- src/components/Main/ItemDetail.js
+++ src/components/Main/ItemDetail.js
@@ -5,10 +5,10 @@ export const ItemDetail = (props) => {
     return(
     <>
         <img className="detail-img" src={props.item.img}></img>
-        <h3 className="detail-h3">Caracteristicas</h3>
         <p className="detail-descripcion">{props.item.caracteristica}</p>
         <p className="detail-descripcion">{props.item.precio}$</p>
         <ItemCount initial="1" stock="10"/>
-        </>
+        <button className="detail-button">Agregar al carrito</button>
+    </>
     )
 }
