0b10d8c9f61cb707c59b2b8d5ad78ef8aa1c68c2
tato <tato.ocampo@live.com.ar>
2022-09-12 16:28:28 -0300

    Eventos

--- src/components/Main/ItemDetail.js
+++ src/components/Main/ItemDetail.js
@@ -1,11 +1,15 @@
 import "./Main.css"
 import { ItemCount } from "./ItemCount"
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 export const ItemDetail = (props) => {
 
+    let [compra, setCompra] = useState(false)
+
     const onAdd = (msg) => {
-        console.log(`Compro ${msg} ${props.item.tipo}`);
+        console.log(`Compro ${msg} ${props.item.tipo}`)
+        setCompra(true)
     }
 
     return(
@@ -16,7 +20,7 @@ export const ItemDetail = (props) => {
         <p class="detail-descripcion">{props.item.precio}$</p>
         <ItemCount initial="1" stock="10" onAdd={onAdd}/>
     </div>
-    <Link to="/cart"><button class="finalizar-button">Finalizar Compra</button></Link>
+    <Link to="/cart"><button hidden={!compra} class="finalizar-button">Finalizar Compra</button></Link>
     </>
     )
 }
